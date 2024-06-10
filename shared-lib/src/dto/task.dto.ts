36bed3657dcebd010f77ad49f78d5e6849080b0c
Valery Semenenko <gearmobile@gmail.com>
2024-06-10 11:40:19 +0300

    fix table error

--- shared-lib/src/dto/task.dto.ts
+++ shared-lib/src/dto/task.dto.ts
@@ -1,8 +1,14 @@
-import { IsBoolean, IsDate, IsNumber } from 'class-validator';
+import { IsBoolean, IsDate, IsNumber, IsString } from 'class-validator';
 
 export class TaskDto {
   @IsNumber()
   id!: number;
+
+  @IsString()
+  title!: string;
+
+  @IsString()
+  content!: string;
 
   @IsBoolean()
   completed!: boolean;
--- shared-lib/src/interface/task.interface.ts
+++ shared-lib/src/interface/task.interface.ts
@@ -1,5 +1,7 @@
 export interface ITask {
   id: number;
+  title: string;
+  content: string;
   completed: boolean;
   createdAt: Date;
   updatedAt: Date;
